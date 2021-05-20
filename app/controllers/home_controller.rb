0f17e50934af4562474a8138609791b88e670cb9
Raymart Prado <prado.raymart0121@gmail.com>
2021-05-20 09:57:50 +0800

    MR-6 Post Edit and Soft delete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.includes(:user)
+    @posts = Post.includes(:user).where(deleted: false).order("created_at DESC")
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :destroy]
+  
   def index
   end
 
@@ -14,7 +16,42 @@ class PostsController < ApplicationController
     end
   end
   
-  def view
+  def show
     @posts = Post.includes(:user).find(params[:id])
   end
+  
+  def edit
+  end
+  
+  def update
+    respond_to do |format|
+      if @post.update(
+        user_id: current_user.id,
+        content: params[:post][:content],
+        deleted: false
+      )
+        format.html { redirect_to root_path, notice: 'Post was successfully updated.' }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      if @post.update(
+        deleted: true
+      )
+        format.html { redirect_to root_path, notice: 'Post was successfully deleted.' }
+        format.json { head :no_content }
+      end
+    end
+  end
+  
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
 end
