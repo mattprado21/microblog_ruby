41389488a66a9d644b30b79d01f95af2c3a42eb6
Raymart Prado <prado.raymart0121@gmail.com>
2021-06-03 17:48:10 +0800

    MR-18 Followers and Following pages, search user and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,17 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :likes, dependent: :destroy
     has_many :comments, -> { where(deleted: false) }
-    
+
     has_one_attached :image
-    
+
+    # Pagination
+    self.per_page = 5
+
+    def self.search(search)
+        # Title is for the above case, the OP incorrectly had 'name'
+        where("content LIKE ?", "%#{search}%")
+    end
+
     # Validations
     validates :content, presence: true, length: { maximum: 140 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_013813) do
+ActiveRecord::Schema.define(version: 2021_05_26_020505) do
 
   create_table "active_storage_attachments", charset: "latin1", force: :cascade do |t|
     t.string "name", null: false
@@ -88,6 +88,16 @@ ActiveRecord::Schema.define(version: 2021_05_24_013813) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "relationships", charset: "latin1", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
   create_table "users", charset: "latin1", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -102,7 +112,6 @@ ActiveRecord::Schema.define(version: 2021_05_24_013813) do
     t.string "gender"
     t.date "birthdate"
     t.string "bio"
-    t.string "profile_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
